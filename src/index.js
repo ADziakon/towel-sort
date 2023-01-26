1e555919851eef2069f9ca1edd76e587ee963cc9
ADziakon <a.dziakanchuk@gmail.com>
2023-01-26 23:22:49 +0300

    feat: add js code task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr=[];
+  if(arguments.length === 0){
+    return [];
+  }else{
+    if(matrix !== []){
+      for(i=0;i<matrix.length;i++){
+        if(i%2 == 0){
+          for(j = 0;j<matrix[i].length;j++){
+            arr.push(matrix[i][j]);
+          }
+        }else{
+          for(l = matrix[i].length-1; l>=0;l--){
+            arr.push(matrix[i][l]);
+          }
+        }
+      }
+  }
+    return arr;
+  }
+
 }
